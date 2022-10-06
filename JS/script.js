5e29217191558fadb350f7e787ef60973884aed5
beirig0 <sarahcbeirigo@gmail.com>
2022-10-06 11:05:59 -0300

    .

--- JS/script.js
+++ JS/script.js
@@ -37,42 +37,6 @@ function receba (){
 }
 
 
-// function receba() {
-//     var btnSalvar = document.querySelector("#btnSalvar");
-
-//     btnSalvar.addEventListener('click', function (event) {
-//         event.preventDefault();
-
-//         var form = document.querySelector("#formulario");
-//         // console.log(form.qtde.value);
-
-
-//     });
-
-//     let qtdeDigitada = document.querySelector("#formulario").value;
-
-//     let arroz = {
-//         kcal: 1,
-//         proteina: 0.1,
-//         carboidrato: 0.2,
-//         gordura: 0.1,
-//     }
-
-//     function somaKcal(qtdeDigitada, arroz) {
-//         let kcal = arroz.kcal;
-//         let proteina = arroz.proteina;
-//         let carboidrato = arroz.carboidrato;
-//         let gordura = arroz.gordura;
-//         return;
-
-
-//     }
-
-//     somaKcal(qtdeDigitada);
-//     alert(`${quatidade}g de arroz tem: \n${kcal * quatidade} Kcal\n${proteina * quatidade}g Proteina\n${carboidrato * quatidade}g Carboidrato\n${gordura * quatidade}g Gordura`);
-
-// };
-
 
 
 
